041b5c4a9b6f4ccd7a9a7e48fc77455afbd79b83
ahmedalaa3307023 <ahmedalaa3307023@gmail.com>
2014-08-27 16:11:19 +0300

    uploading existing files

---
+++ application/controllers/welcome.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo 'hello ahmed';
+	}
+	
+	public function hello()
+	{
+		echo 'hello alaa';
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/controllers/welcome_controller.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class welcome_controller extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('downloading_page');
+	}
+	
+	public function hello()
+	{
+		echo "hello mohamed";
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
